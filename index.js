40af147e8128d8a29d9c6685670d2060dd34498e
parm530 <parmsahadeo530@gmail.com>
2017-01-05 10:51:51 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time: Math.round(new Date().getTime()/1000),
+  startTicking: function () {
+    setInterval( () => this.time++, 1000)
+  }
+};
+
+
+function censor(word, string) {
+  while(string.includes(word)) {
+    string = string.replace(word, "BLEEP");
+  }
+  return string;
+};
+
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor  = censor.bind(null, 'drugs');
